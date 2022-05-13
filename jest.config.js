656fb1cccab804fff0abdbde749f16561fe69da6
Mohammad Ahtasham ul Hassan <ahthassan74@gmail.com>
2022-05-13 17:57:15 +0500

    fix: fix misc changes

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,18 @@
 module.exports = {
     "globals": {
-      "gettext": (t) => { return t; },
+        "gettext": (t) => { return t; },
     },
     "modulePaths": [
-      "common/static/common/js/components",
+        "common/static/common/js/components",
     ],
     "setupFilesAfterEnv": ["<rootDir>/setupTests.js"],
     "testMatch": [
-      "/**/*.test.jsx",
-      "common/static/common/js/components/**/?(*.)+(spec|test).js?(x)",
+        "/**/*.test.jsx",
+        "common/static/common/js/components/**/?(*.)+(spec|test).js?(x)",
     ],
     "testEnvironment": "jsdom",
     "transform": {
-      "^.+\\.jsx$": "babel-jest",
-      "^.+\\.js$": "babel-jest",
+        "^.+\\.jsx$": "babel-jest",
+        "^.+\\.js$": "babel-jest",
     },
-  };
-  
+};
